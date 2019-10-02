8a39620be5e479b5034acb709d9f66714eae092a
capstone <capstone@WSD-110411.ad.windhamsd.org>
2019-10-02 13:50:47 -0400

    motor testing

--- open-flightware/src/main/java/adamf59/SystemHostController/Subsystems/Avionics/Avionics.java
+++ open-flightware/src/main/java/adamf59/SystemHostController/Subsystems/Avionics/Avionics.java
@@ -5,38 +5,59 @@
 */
 package adamf59.SystemHostController.Subsystems.Avionics;
 
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioPinAnalog;
+import com.pi4j.io.gpio.GpioPinAnalogOutput;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.GpioPinPwmOutput;
+import com.pi4j.io.gpio.Pin;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.util.CommandArgumentParser;
+import com.pi4j.wiringpi.Gpio;
+import com.pi4j.wiringpi.SoftPwm;
+
 import adamf59.Core.Subsystem;
 import adamf59.SystemHostController.SystemHost;
+import adamf59.SystemHostController.IO.GPIO;
 import adamf59.SystemHostController.System.Console;
 
 public class Avionics extends Subsystem {
-
+    Pin pin;
+    GpioPinPwmOutput pwm;
     public Avionics(int id) {
         super("JAGSAT_AVIONICS_SUBSYSTEM", id);
-
+        
     }
 
     @Override
     public void init() {
         Console.printInfo("Initializing Avionics Subsystem");
-
+        pin = CommandArgumentParser.getPin(RaspiPin.class, RaspiPin.GPIO_01);
+        pwm = GPIO.getGPIOController().provisionPwmOutputPin(pin);
+        Gpio.pwmSetMode(Gpio.PWM_MODE_MS);
+        Gpio.pwmSetRange(1000);
+        Gpio.pwmSetClock(500);
     }
 
 
     @Override
     public void execute() {
             Console.printInfo("Avionics subsystem updating...");
+            setPwm(100);
             try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        setPwm(0);
     }
 
     @Override
     public void executeAlways() {
         
     }
-
-
+    public void setPwm(int rate){
+        pwm.setPwm(rate);
+    }
 }
