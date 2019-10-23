45ab40df1327f2bcf808a70155e85be6214fde3a
capstone <capstone@WSD-110424.ad.windhamsd.org>
2019-10-23 13:29:18 -0400

    test

--- open-flightware/src/main/java/adamf59/SystemHostController/Subsystems/Avionics/Avionics.java
+++ open-flightware/src/main/java/adamf59/SystemHostController/Subsystems/Avionics/Avionics.java
@@ -21,21 +21,23 @@ import com.pi4j.wiringpi.SoftPwm;
 
 import adamf59.Core.Subsystem;
 import adamf59.SystemHostController.SystemHost;
-import adamf59.SystemHostController.IO.GPIO;
+import adamf59.SystemHostController.IO.*;
 import adamf59.SystemHostController.System.Console;
 
 public class Avionics extends Subsystem {
     Pin pin;
     ESCControl esc;
     GpioPinPwmOutput pwm;
+    SerialArduino arduino;
     public Avionics(int id) {
         super("JAGSAT_AVIONICS_SUBSYSTEM", id);
-        
+        arduino = new SerialArduino();
     }
 
     @Override
     public void init() {
         Console.printInfo("Initializing Avionics Subsystem");
+        arduino.init();
 /*         pin = CommandArgumentParser.getPin(RaspiPin.class, RaspiPin.GPIO_01);
         pwm = GPIO.getGPIOController().provisionPwmOutputPin(pin);
         
@@ -50,16 +52,15 @@ public class Avionics extends Subsystem {
        setPwm(0);
        Scanner scan1 = new Scanner(System.in);
        String in1 = scan.next(); */
-       esc = new ESCControl(18);
-       esc.calibrate();
-       esc.arm(true);
+       
     }
 
 
     @Override
     public void execute() {
-            Console.printInfo("Avionics subsystem updating...");
-           esc.fullSpeed();
+        Scanner scan = new Scanner(System.in);
+        String in = scan.next();
+        arduino.sendSerial(in);
             
             
     }
